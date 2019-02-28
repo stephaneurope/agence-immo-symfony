e0e1f9b0f0585e964fda7162a6a85742590df8d7
serri stephan <serri.stephan@gmail.com>
2019-02-28 08:59:00 +0100

    Gestion des annonces, formulaire de creation et edition

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,21 +7,24 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
-class HomeController extends Controller {
+class HomeController extends Controller
+{
 
     /**
      * @route("/", name="homepage")
      */
 
-    public function home(){
+    public function home()
+    {
         $prenom = ["Lior", "Joseph", "Anne"];
-       return $this->render(
-      'home.html.twig',
-      ['title' => "Bonjour a tous",
-       'age'   => 31,
-       'tableau' => $prenom
-       ]
-       );
+        return $this->render(
+            'home.html.twig',
+            [
+                'title' => "Bonjour a tous",
+                'age' => 31,
+                'tableau' => $prenom
+            ]
+        );
     }
 }
 
